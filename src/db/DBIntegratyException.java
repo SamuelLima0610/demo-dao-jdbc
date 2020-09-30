d9ad64a4df52bfe4d4edb2b32f25cc95736f7d03
Samuel Vitorio Lima <34277130+SamuelLima0610@users.noreply.github.com>
2020-09-30 14:20:15 -0300

    Creating project and git repository

---
+++ src/db/DBIntegratyException.java
@@ -0,0 +1,13 @@
+package db;
+
+public class DBIntegratyException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DBIntegratyException(String message) {
+		super(message);
+	}
+}
